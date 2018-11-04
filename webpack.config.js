dd85a56169c9a283880480e712679096ad22fcc1
Lonefort <stadnik.yuri@gmail.com>
2018-11-04 23:46:13 +0200

    webpack dev server change entry

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   devtool: 'eval',
   mode: 'development',
   entry: [
-    // 'webpack-dev-server/client?http://localhost:3000',
+    'webpack-dev-server/client?http://localhost:3000',
     './src/index.jsx'
   ],
   devServer:{
